1e5f492f2adc95009a9e0c4860337bafe9057f98
Osvaldo Gago <opgago@yahoo.com>
2022-10-25 07:53:30 +0100

    Change function to use API v3 and private app token

--- includes/functions.php
+++ includes/functions.php
@@ -2,7 +2,10 @@
 
 function httpGet($email) {
 	$ch = curl_init();
-	curl_setopt($ch,CURLOPT_URL,'https://api.hubapi.com/contacts/v1/contact/email/'. $email . '/profile?hapikey='. HUBSPOTAPIKEY);
+	curl_setopt($ch,CURLOPT_URL,'https://api.hubapi.com/crm/v3/objects/contacts/'. $email . '?idProperty=email&properties=lifecyclestage&properties=phone&properties=mobilephone&properties=otp_out_hbs&properties=salesforcecampaignids');
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+		'authorization: Bearer ' . HUBSPOT_PRIVATE_APP_KEY,
+	));
 	curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
